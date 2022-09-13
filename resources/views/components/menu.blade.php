f89611d328b502522e486cb0ece1026626bfcdef
TadeoITLP <tadeo.rs@lapaz.tecnm.mx>
2022-09-13 07:39:44 -0600

    Menu

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,114 @@
+<nav>
+    <ul>
+        <li>
+            <img src="/storage/img/computadoras.png">
+            <span>Computadoras</span>
+            <ul>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Laptops</a>
+                </li>
+                <li>
+                    <img src="">
+                    <a href="">Smartphones</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Desktops</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Tablets</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <img src="/storage/img/computadoras.png">
+            <span>hardware</span>
+            <ul>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Tarjetas de video</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Discos duros</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Procesadores</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Memorias</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <img src="/storage/img/computadoras.png">
+            <span>Accesorios</span>
+            <ul>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Teclados</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Mouses</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Bocinas</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Sillas</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <img src="/storage/img/computadoras.png">
+            <span>Almacenamiento</span>
+            <ul>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Flash</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Discos internos</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Discos externos</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">SSD</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <img src="/storage/img/computadoras.png">
+            <span>Electronica</span>
+            <ul>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Monitores</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Televisores</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Proyectores</a>
+                </li>
+                <li>
+                    <img src="/storage/img/computadoras.png">
+                    <a href="">Camaras</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</nav>
